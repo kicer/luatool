e987e04da431b77d0f2bb7742ac9818bf51601ea
Kicer <joykicer@gmail.com>
2018-12-19 23:56:55 +0800

    fix main.lua for uart console

--- luatool/main.lua
+++ luatool/main.lua
@@ -1,3 +1,6 @@
+PROJECT='console'
+VERSION='1.0.0'
+
 local uart_id = 2
 local baudrate = 115200
 
@@ -11,22 +14,30 @@ local print = function(...)
 end
 _G.print = print
 
-uart.setup(uart_id, baudrate, 8, uart.PAR_NONE, uart.STOP_1, 0, 0)
+uart.setup(uart_id, baudrate, 8, uart.PAR_NONE, uart.STOP_1)
 
-uart.on(uart_id, 'receive', function()
-    local line = uart.read(uart_id, '*l')
-    uart.write(uart_id, line)
-    uart.write(uart_id, '\n')
-    xpcall(function()
-      local f = assert(loadstring(line))
-      f()
-      uart.write(uart_id, '\r\n# ')
-    end, function(e)
-      uart.write(uart_id, e)
-      uart.write(uart_id, '\r\n')
-      uart.write(uart_id, debug.traceback())
-    end)
-end)
+uart.write(uart_id, 'Welcome to console.\r\n> ')
 
-uart.write(uart_id, 'Welcome to console.\r\n# ')
-
+local line = ''
+while true do
+    local ch = uart.read(uart_id, 1)
+    if ch and ch~='' then
+        line = line .. ch
+        if ch == '\n' then
+            uart.write(uart_id, line)
+            uart.write(uart_id, '\n')
+            xpcall(function()
+              local f = assert(loadstring(line))
+              f()
+            end, function(e)
+              uart.write(uart_id, e)
+              uart.write(uart_id, '\r\n')
+              uart.write(uart_id, debug.traceback())
+            end)
+            line = ''
+            uart.write(uart_id, '\r\n> ')
+        end
+    else
+        rtos.sleep(100)
+    end
+end
